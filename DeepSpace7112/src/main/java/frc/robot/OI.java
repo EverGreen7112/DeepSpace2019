7e22b26f24a0b075c72c0eec8216be24d8969941
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2019-01-15 20:53:08 +0200

    Updated OI

--- DeepSpace7112/src/main/java/frc/robot/OI.java
+++ DeepSpace7112/src/main/java/frc/robot/OI.java
@@ -7,7 +7,11 @@
 
 package frc.robot;
 
+import com.spikes2212.genericsubsystems.basicSubsystem.commands.MoveBasicSubsystem;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -20,11 +24,39 @@ public class OI {
   //----------Joysticks----------
   private Joystick drivingJSLeft;
   private Joystick drivingJSRight;
+  private Joystick buttonJS; 
+
+  //----------Buttons----------  
+  private Button A;
+  private Button Y;
+	//--------------------Constructors--------------------
+
+	public OI(){
+
+		  //----------Joysticks----------
+		drivingJSLeft = new Joystick(0);
+		drivingJSRight = new Joystick(1);
+		buttonJS = new Joystick(2);
+
+		  //----------Buttons----------
+		A = new JoystickButton(buttonJS, 2);
+		Y = new JoystickButton(buttonJS, 4);
+
+		bindButtons();
+	}
 
 	//--------------------Methods--------------------
 	
+	private void bindButtons(){
+
+		//Lowers the climbing shaft until reaching the bottom microswitch
+		A.whileHeld(new MoveBasicSubsystem(Robot.shaft, -SubsystemConstants.shaftMotorSpeedModifier.get()));
+		//Raises the climbing shaft until reaching the top microswitch
+		Y.whileHeld(new MoveBasicSubsystem(Robot.shaft, SubsystemConstants.shaftMotorSpeedModifier.get()));
+	}
+	
   	// receives input, returns the adjusted input for better sensitivity
-		private double adjustInput(double input){
+	private double adjustInput(double input){
 			return input * Math.abs(input);
     }
     
