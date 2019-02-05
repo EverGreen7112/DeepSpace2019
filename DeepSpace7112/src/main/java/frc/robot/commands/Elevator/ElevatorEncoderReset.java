77d67b7ca9fa44ddee1729da2ad567b8cb25ecb1
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2019-02-05 22:46:47 +0200

    Changed from command group to command

--- DeepSpace7112/src/main/java/frc/robot/commands/Elevator/ElevatorEncoderReset.java
+++ DeepSpace7112/src/main/java/frc/robot/commands/Elevator/ElevatorEncoderReset.java
@@ -7,16 +7,47 @@
 
 package frc.robot.commands.Elevator;
 
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 import frc.robot.SubsystemComponents;
 
-public class ElevatorEncoderReset extends CommandGroup {
-  /**
-   * Add your docs here.
-   */
+/**
+ * Resets the encoder when the optic switch is triggered
+ */
+public class ElevatorEncoderReset extends Command {
   public ElevatorEncoderReset() {
+    requires(Robot.elevator);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
     if(SubsystemComponents.Elevator.opticSwitch.get()){
       SubsystemComponents.Elevator.encoder.reset();
     }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    end();
   }
 }
