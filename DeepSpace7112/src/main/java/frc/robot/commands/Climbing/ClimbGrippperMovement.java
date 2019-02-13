4164327d2fa0a1faf857366f57dc6165a3189692
OGPhoenixRider <45061386+OGPhoenixRider@users.noreply.github.com>
2019-02-13 17:15:17 +0200

    Starts the commands for the climbing auto movement
    adds the commands needed and short explanations on what needs to be done

---
+++ DeepSpace7112/src/main/java/frc/robot/commands/Climbing/ClimbGrippperMovement.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ClimbGrippperMovement extends Command {
+  public ClimbGrippperMovement() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    //This needs to lower the gripper onto the platform (alone) and then continue to push down with the back frame to raise the robot
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
