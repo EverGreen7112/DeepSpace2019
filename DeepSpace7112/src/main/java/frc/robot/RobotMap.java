18771c50531c4b32b5e6c1cc610fb0cbb00ec1e2
EverGreen7112 <34937866+EverGreen7112@users.noreply.github.com>
2019-03-02 13:16:17 +0200

    Revert "Commit to Revert (ignore)"
    This reverts commit bbdcfde80bed5125a5346c71229ae014a111ea3e.

--- DeepSpace7112/src/main/java/frc/robot/RobotMap.java
+++ DeepSpace7112/src/main/java/frc/robot/RobotMap.java
@@ -14,49 +14,48 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  
-  //----------Chassis----------
+//----------Chassis----------
   public static final int 
-              chassisVictorFL = 7, //CAN
-              chassisVictorBL = 8, //CAN
+              chassisVictorFL = 8, //CAN
+              chassisVictorBL = 5, //CAN
               chassisVictorFR = 2, //CAN
-              chassisVictorBR = 5; //CAN
-//all temp
+              chassisVictorBR = 7; //CAN
+              //all temp
   
-  //----------cameras----------
-  public static final int
-              cameraA = 0,
-              cameraB = 1;
+//----------cameras----------
+    public static final int
+                cameraA = 0,
+                cameraB = 1;  //All temp
   
-  //----------Elevator----------
+//----------Elevator----------
   public static final int  
-              elevatorMotorL = 3, //CAN
-              elevatorMotorR = 4, //CAN
+              elevatorMotorF = 3, //CAN
+              elevatorMotorB = 6, //CAN
               elevatorEncoderA = 0, //DIO
               elevatorEncoderB = 1, //DIO
               elevatorOpticSwitch = 6, //DIO
               elevatorLazerDistanceSensor = 1; //Analog
-              //All temp
+              //All tem
 
-//----------ClimbingShaft----------
-public static final int 
-            shaftTalon = 6, //CAN
-            shaftBottomLimiter = 2; //DIO
+//----------Climbing Frame----------
+  public static final int 
+              frameTalon = 1, //CAN
+              frameBottomLimiter = 3; //DIO
 
 //----------Climbing Movement----------
-public static final int 
-              climbingMovementMotor = 1, //CAN
-              climbingMovementEncoderA = 1, //DIO
-              climbingMovementEncoderB = 2; //DIO
+  public static final int 
+                climbingMovementMotor = 0, //CAN
+                climbingMovementEncoderA = 1, //DIO
+                climbingMovementEncoderB = 2; //DIO
 
 //----------Gripper----------
   public static final int 
-            gripperMotorLeft = 6, //CAN
-            gripperMotorRight = 7, //CAN
-            gripperLeftPistonF= 0, //Left Forward
-            gripperLeftPistonR= 1, //Left Reverse
-            gripperRightPistonF = 2, //Right Forward
-            gripperRightPistonR = 3, //Right Reverse
+            gripperMotorLeft = 9  , //CAN
+            gripperMotorRight = 4, //CAN
+            gripperLeftPistonF= 0, //Left Forward -TEMP-
+            gripperLeftPistonR= 1, //Left Reverse -TEMP-
+            gripperRightPistonF = 2, //Right Forward -TEMP-
+            gripperRightPistonR = 3, //Right Reverse -TEMP-
             gripperAnalogLazerSensor = 5; //analog
             
 //----------Gripper Movement----------
@@ -65,7 +64,7 @@ public static final int
             gripperMovementLeftPistonR = 1, //Left Reverse
             gripperMovementRightPistonF = 2, //Left Forward
             gripperMovementRightPistonR = 3, //Left Reverse
-            gripperMovementTopMicroswitch = 4,
-            gripperMovementBottomMicroSwitch = 5;
+            gripperMovementTopMicroswitch = 4, 
+            gripperMovementBottomMicroSwitch = 5; 
 }
 
